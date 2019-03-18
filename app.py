ceb7bcf72db007bc6390732d325e8d1714a502e4
Matt Freeland <matthewjfreeland@gmail.com>
2019-03-18 14:31:52 -0700

    initial commit with starting routes added

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+DEBUG = True
+PORT = 8000
+
+app = Flask(__name__)
+app.secret_key = "potato"
+
+@app.route('/')
+def index():
+    return("Project3 - Ready to Rock")
+
+@app.route('/dashboard')
+def dashboard():
+    return("Dashboard here")
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG, port=PORT)
