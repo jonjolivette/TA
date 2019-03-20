105c5b0af827bf5b65e8766c8d68fbd5afdd99d0
JJ <johnhenry@JJs-MacBook-Pro.local>
2019-03-20 13:58:39 -0700

    added landing page, nav bar, templates, and assests

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_fontawesome import FontAwesome
 
 # from flask import render_template
-
-DEBUG = True
-PORT = 8000
-
 app = Flask(__name__)
-app.secret_key = 'radish'
+Bootstrap(app)
+
+# ============ HOME PAGE ROUTE ============
+@app.route('/')
+def index():
+    return render_template('hero.html')
+
+# ============ STUDENT DASHBOARD ROUTE ============
+@app.route('/student')
+def student_dash():
+    return render_template('student-dashboard.html')
+
+
+# ============ TEACHER DASHBOARD ROUTE ============
+@app.route('/teacher')
+def teacher_dash():
+    return render_template('teacher-dashboard.html')
 
 if __name__ == '__main__':
-    app.run(debug=DEBUG, port=PORT)
+    app.secret_key = 'radish'
+    app.run(debug=True, port=8000)
