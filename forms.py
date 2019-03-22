ebde89773f572c45c5050ac1b4c52a0ef220810f
e-morales <e.morales@digitalnest.org>
2019-03-22 10:09:58 -0700

    added a new branch/fresh start. created templates for login and register with user feedback

--- forms.py
+++ forms.py
@@ -1,74 +1,18 @@
-from flask_wtf import FlaskForm as Form
-
-from models import User
-
-from wtforms import StringField, PasswordField, TextAreaField, DateField, SelectField
-from wtforms.validators import (DataRequired, Regexp, ValidationError, Email,
-                                Length, EqualTo)
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-def name_exists(form, field):
-    if User.select().where(User.username == field.data).exists():
-        raise ValidationError('User with that name already exists.')
-
-
-def email_exists(form, field):
-    if User.select().where(User.email == field.data).exists():
-        raise ValidationError('User with that email already exists.')
-
-
-class RegisterForm(Form):
-    username = StringField(
-        'Name',
-        validators=[
-            DataRequired(),
-            Regexp(
-                r'^[a-zA-Z ]+$',
-                message=("Name cannot contain symbols or special characters")
-            ),
-            name_exists
-        ])
-    email = StringField(
-        'Email',
-        validators=[
-            DataRequired(),
-            Email(),
-            email_exists
-        ])
-    password = PasswordField(
-        'Password',
-        validators=[
-            DataRequired(),
-            Length(min=2),
-            EqualTo('password2', message='Passwords must match')
-        ])
-    password2 = PasswordField(
-        'Confirm Password',
-        validators=[DataRequired()]
-    )
-
-# class DeleteUserForm(Form):
-
-
-class CreateEventForm(Form):
-    date = DateField(
-        'Date',
-        validators=[
-            DataRequired()
-        ],
-        format="%Y-%m-%d"
-    )
-    duration = SelectField(
-        'Duration',
-        choices=[("15", '15 Minutes'), ("30", '30 Minutes'),
-                 ("45", '45 Minutes')],
-        default="15"
-    )
-
-
-class LoginForm(Form):
+class RegistrationForm(FlaskForm):
+    # means data is required and the min and max for it
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
-# class PostForm(Form):
-#     content = TextAreaField("Enter Post here", validators=[DataRequired()])
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
